8adaaa1aadb1e84d5f739aaa89671d98ebaf1323
lxhao <lxhao580@163.com>
2016-06-18 12:24:47 +0800

    改为相对路径

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import os,random,commands
+import os.path as op
 
-basedir = "/mnt/code/code/python/cleanBingDesktop/wallpapers/";
-filename = basedir + random.choice(os.listdir('/mnt/code/code/python/cleanBingDesktop/wallpapers'))
+basedir = op.join(op.abspath(op.dirname(__file__)), 'wallpapers')
+filename = basedir + '/' +  random.choice(os.listdir(basedir))
 print filename
 commands.getstatusoutput('DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri file://"%s"' % (filename))
